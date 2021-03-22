c6b780536e6e4db307105d0374af1debded34a36
yining wang <ywang@lcs.on.ca>
2021-03-21 20:39:59 -0400

    Initial Commit

---
+++ final assignment experiment/final_assignment_experimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  final_assignment_experimentApp.swift
+//  final assignment experiment
+//
+//  Created by Yining Wang on 2021-03-21.
+//
+
+import SwiftUI
+
+@main
+struct final_assignment_experimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
