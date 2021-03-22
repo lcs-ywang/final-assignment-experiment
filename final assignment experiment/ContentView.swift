be7bc0862cf50bf3767e29ab8dd53991c68be9ce
yining wang <ywang@lcs.on.ca>
2021-03-22 11:01:33 -0400

    Add content view with details

--- final assignment experiment/ContentView.swift
+++ final assignment experiment/ContentView.swift
@@ -5,3 +5,99 @@
 //  Created by Yining Wang on 2021-03-21.
 //
 
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationView{
+            
+            List{
+                NavigationLink(destination: Breakfast()){
+                    HStack{
+                        
+                        Text("🥞")
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading){
+                            Text("Breakfast")
+                                .multilineTextAlignment(.leading)
+                            
+                            Text("The most significant meal for a day.")
+                                .font(.caption)
+                                .multilineTextAlignment(.leading)
+                            
+                        }
+                    }
+                }
+                
+                NavigationLink(destination: Lunch()){
+                    HStack{
+                        
+                        Text("🍱")
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading){
+                            Text("Lunch")
+                                .multilineTextAlignment(.leading)
+                            
+                            Text("Lunch is important for the classes in the afternoon.")
+                                .font(.caption)
+                                .multilineTextAlignment(.leading)
+                            
+                        }
+                    }
+                }
+                
+                NavigationLink(destination: Dinner()){
+                    HStack{
+                        
+                        Text("🍛")
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading){
+                            Text("Dinner")
+                                .multilineTextAlignment(.leading)
+                            
+                            Text("Dinner is delicious today.")
+                                .font(.caption)
+                                .multilineTextAlignment(.leading)
+                            
+                        }
+                    }
+                }
+                
+               
+                NavigationLink(destination: Dinein_or_Takeout()){
+                    HStack{
+                        
+                        Text("🍽")
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading){
+                            Text("Dine-in or Take-out")
+                                .multilineTextAlignment(.leading)
+                            
+                            Text("Figure it out whether Dine-in or Take-out")
+                                .font(.caption)
+                                .multilineTextAlignment(.leading)
+                            
+                        }
+                    }
+                    .padding()
+                    
+                }
+                
+            }
+            .navigationBarTitle("LCS Meal")
+            
+        }
+        
+    }
+    
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
